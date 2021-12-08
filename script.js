0de969bc0582d1de34a84b79961a5e32e5ec8c10
Shithappe <greenmad.b50@gmail.com>
2021-12-08 16:49:48 +0300

    save in localStorage

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const Form = document.getElementById('form');
 
 function AddElement(){
-
     Form.classList.remove("formHidde");
     Form.classList.add("formShow");
 }
@@ -9,9 +8,26 @@ function AddElement(){
 function cancelForm(){
     Form.classList.remove("formShow");
     Form.classList.add("formHidde");
+
+    console.log(localStorage.getItem('allEl'));
 }
 
 function saveEl(){
-    let name = document.getElementById('tab-1').value;
-    console.log(name);
+
+    let newEl = {
+        name: document.getElementById('name').value,
+        link: document.getElementById('link').value,
+        price: document.getElementById('price').value,
+        done: false
+    }
+
+    let allEl = [];
+    if (localStorage.getItem('allEl')) allEl = JSON.parse(localStorage.getItem('allEl'));
+
+    allEl.push(newEl);
+
+
+    localStorage.setItem('allEl', JSON.stringify(allEl));
+    // console.log(localStorage.getItem('allEl'));
+    cancelForm();
 }
