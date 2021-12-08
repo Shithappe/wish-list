b059ef3c2e0ae853880e15fafdc6e0216ae7d12d
Shithappe <greenmad.b50@gmail.com>
2021-12-08 22:26:51 +0300

    almost copy and fix bug with add new item

--- script.js
+++ script.js
@@ -12,10 +12,26 @@ function viewList(){
         let div = document.createElement('div');
         div.className = "item";
         div.innerHTML = '<h3>' + allEl[i].name + '</h3>' + 
-                        '<div>' + allEl[i].link + '<button>copy</button>' + '</div>' + '<br>' + allEl[i].price;
-        
+                        '<div>' + allEl[i].link + '</div>' + '<br>' + allEl[i].price;
+        // div.addEventListener('click', setdata, false);
+        div.setAttribute('id', i);
+        let copyBtn = document.createElement('button');
+        copyBtn.onclick = getParentElemId;
+        copyBtn.innerText = 'copy';
+        div.appendChild(copyBtn);
         document.body.append(div);
     }  
+}
+
+function getParentElemId() {
+    // console.log(this);
+    let parentEl = this.parentNode;
+    console.log(parentEl.id);
+    document.execCommand("copy");//no..
+
+}
+function setdata() {
+    console.log(this.id);
 }
 
 function AddElement(){
@@ -46,6 +62,12 @@ function saveEl(){
 
 
     localStorage.setItem('allEl', JSON.stringify(allEl));
-    // console.log(localStorage.getItem('allEl'));
+    
+    let div = document.createElement('div');
+        div.className = "item";
+        div.innerHTML = '<h3>' + newEl.name + '</h3>' + 
+                        '<div>' + newEl.link + '<button>copy</button>' + '</div>' + '<br>' + newEl.price;
+        document.body.append(div);
+
     cancelForm();
 }
