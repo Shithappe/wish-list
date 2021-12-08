dfd9edda0046024299772c31f2e30cfee27b6594
Shithappe <greenmad.b50@gmail.com>
2021-12-08 18:02:10 +0300

    view elements

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const Form = document.getElementById('form');
 
+viewList();
+
+function viewList(){
+    let allEl = [];
+    if (localStorage.getItem('allEl')) allEl = JSON.parse(localStorage.getItem('allEl'));
+    console.log(allEl);
+
+
+    for (i = 0; i < allEl.length; i++) {
+        let div = document.createElement('div');
+        div.className = "item";
+        div.innerHTML = '<h3>' + allEl[i].name + '</h3>' + 
+                        '<div>' + allEl[i].link + '<button>copy</button>' + '</div>' + '<br>' + allEl[i].price;
+        
+        document.body.append(div);
+    }  
+}
+
 function AddElement(){
     Form.classList.remove("formHidde");
-    Form.classList.add("formShow");
+    Form.classList.add("formShow");    
 }
 
 function cancelForm(){
