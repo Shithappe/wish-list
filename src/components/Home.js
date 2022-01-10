57345f343db239470c984c04a0486fc42199a1d9
Shithappe <greenmad.b50@gmail.com>
2022-01-10 02:47:51 +0200

    data structure change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import Wish from './Wish.js'
 
 export default function Home() {
+    let data = {
+        name: 'max',
+        link: 'http://localhost:3000/home',
+        price: '500$'
+    }
     return (
         <div className='main'>
-            <h1>Wish List</h1>
-            <Wish name='max' link='http://localhost:3000/home' price='500$' />
-            <Wish name='max' link='http://localhost:3000/home' price='500$' />
+            <h1>Wish List<span>.react</span></h1>
+            <Wish data={data} />
+            <Wish data={data} />
         </div>
     )
 }
--- src/components/Wish.js
+++ src/components/Wish.js
@@ -1,10 +1,11 @@
 export default function Wish(props) {
     return (
         <div className="listItem">
-            <h3>{props.name}</h3>
+            <h3>{props.data.name}</h3>
             <button className="doneButton"></button>
-            <h4>{props.link}</h4>
-            <h4>{props.price}</h4>
+            <h4>{props.data.link}</h4>
+            <button className="copyLinkButton">copy</button>
+            <h4>{props.data.price}</h4>
         </div>
     )
 }
