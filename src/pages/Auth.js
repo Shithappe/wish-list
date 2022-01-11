a4e0147ec8b9c836e7a9a7827f46897413190c88
Shithappe <greenmad.b50@gmail.com>
2022-01-11 15:48:41 +0200

    validation

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -3,7 +3,10 @@ import { useForm } from "react-hook-form";
 
 export default function Home() {
     const { register, handleSubmit, formState: {errors} } = useForm();
-    console.log(errors);
+    // console.log(errors);
+    // console.log(watch());
+    // const pass = watch('password');
+
 
     function Login(){
         return(
@@ -12,8 +15,8 @@ export default function Home() {
                             console.log(data);
                         })}>
                             <h1>Login</h1>
-                            <input {...register("username", {required: true, minLength: 3})} type='text' placeholder="Username" />
-                            <input {...register("password")} type='text' placeholder="Password" />
+                            <input {...register("username", {required: "true", minLength: 3})} type='text' placeholder="Username" />
+                            <input {...register("password")} type='password' placeholder="Password" />
                             <input type='submit' value='Login' />
                             <div className='secondaryButtons'>
                                 <button onClick={() => setMode('register')}>Register</button>
@@ -21,7 +24,6 @@ export default function Home() {
                             </div>
                         </form>
                         <br/>
-                        
                     </div>
         )
     }
@@ -29,12 +31,41 @@ export default function Home() {
     function Register() {
         return(
             <div className="register addItemForm">
-                        <form>
-                        <h1>Register</h1>
-                            <input {...register('username', {required: true, minLength: 3})} type='text' placeholder="Username" />
-                            <input {...register('email', {required: true, minLength: 3})} type='email' placeholder="E-mail" />
-                            <input {...register('password', {required: true, minLength: 3})} type='password' placeholder="Password" />
-                            <input type='text' placeholder="Comf Password" />
+                        <form onSubmit={handleSubmit((data) => {
+                            console.log(data);
+                        })}>
+                            <h1>Register</h1>
+                            <input {...register('username', {
+                                    required: "This is required", 
+                                    minLength: {
+                                        value: 3,
+                                        message: 'min lenght 3'
+                                    }
+                                })} type='text' placeholder="Username" />
+                            {errors.username?.message ? <p className='tipInForm'>{errors.username?.message}</p> : null}
+                            <p>{errors.username?.message}</p>
+
+                            <input {...register('email', {required: "This is required"})} type='email' placeholder="E-mail" />
+                            {errors.email?.message ? <p className='tipInForm'>{errors.email?.message}</p> : null}
+
+                            <input {...register('password1', {
+                                required: "This is required",
+                                minLength: {
+                                    value: 8,
+                                    message: "min lenght 8"
+                                },
+                                pattern: {
+                                    value: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g,
+                                    message: "Must have a number and uppercase"
+                                }
+                            })} type='password' placeholder="Password" />
+                            {errors.password1?.message ? <p className='tipInForm'>{errors.password1?.message}</p> : null}
+                            {errors.password1?.type === "minLength" && "Last name is required"}
+
+                            <input {...register('comfPass', {required: "This is required"})} type='password' placeholder="Confirm Password" />
+                            {/* {errors.comfPass?.value !== errors.password?.value ? <p className='tipInForm'>awd</p> : null} */}
+
+                            <br/>
                             <input type='submit' value='Register' />
                             <div className='secondaryButtons'>
                                 <button onClick={() => setMode('login')}>Login</button>
